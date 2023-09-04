87742e41638b1df397478cb44a3857c331f3a3b9
Martin Bonnin <martin@mbonnin.net>
2023-09-04 16:08:48 +0200

    add suppressions

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,3 +1,5 @@
 public expect class ZDeferred<T>
 
+
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 public expect inline fun <T, S> ZDeferred<T>.then(noinline thenFn: (T) -> S): ZDeferred<S>
--- src/jsMain/kotlin/Main.js.kt
+++ src/jsMain/kotlin/Main.js.kt
@@ -1,8 +1,9 @@
-import kotlin.js.Promise
+import kotlin.js.Promise as Promise1
 
-public actual typealias ZDeferred<T> = Promise<T>
+@Suppress("ACTUAL_TYPE_ALIAS_TO_CLASS_WITH_DECLARATION_SITE_VARIANCE", "ACTUAL_WITHOUT_EXPECT")
+public actual typealias ZDeferred<T> = Promise1<T>
 
-
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 actual inline fun <T, S> ZDeferred<T>.then(noinline thenFn: (T) -> S): ZDeferred<S> {
     TODO("Not yet implemented")
 }
