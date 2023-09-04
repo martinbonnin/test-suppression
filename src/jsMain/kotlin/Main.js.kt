4db8a289c818b3ea2bc699c18284e9e3f391273e
Martin Bonnin <martin@mbonnin.net>
2023-09-04 16:06:30 +0200

    typealias

--- src/jsMain/kotlin/Main.js.kt
+++ src/jsMain/kotlin/Main.js.kt
@@ -1,4 +1,7 @@
-public actual class ZDeferred<T>
+import kotlin.js.Promise
+
+public actual typealias ZDeferred<T> = Promise<T>
+
 
 actual inline fun <T, S> ZDeferred<T>.then(noinline thenFn: (T) -> S): ZDeferred<S> {
     TODO("Not yet implemented")
